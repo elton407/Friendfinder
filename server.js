f15c8eb292dbecfb2d73f018817b1fe3e7dfec65
Elton <user@Elis-MacBook-Pro.local>
2017-08-03 14:28:55 -0400

    heroku install

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var appUsers = [];
 
 // Sets up the Express app to handle data parsing
